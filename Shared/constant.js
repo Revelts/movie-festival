db6446a87ef938f424272b8d622e82e4e835eb9c
Revelt <revelt@lc-rp.mp>
2022-03-08 22:58:28 +0700

    ➕ String Constant

---
+++ Shared/constant.js
@@ -0,0 +1,31 @@
+module.exports = {
+    TITLE_ERROR: 'You must fill Title to insert new movie!',
+    DESCRIPTION_ERROR: 'You must fill Description to insert new movie!',
+    DURATION_ERROR: 'You must fill Duration to insert new movie!',
+    GENRE_ERROR: 'You must fill Genre to insert new movie, atleast 1 genre!',
+    ARTIST_ERROR: 'You must fill atleast 1 Artist to insert new movie!',
+    FILE_ERROR: 'Invalid File Type, you only able to upload mp4, webm, ogg',
+    INSERT_MOVIE_ERROR: 'Something wrong when insert the movie, try again!',
+
+    INVALID_MOST_VIEW_PARAMETER: 'Invalid most view parameter (movies or genres)',
+    INVALID_MOVIE_ID: 'Invalid movie id, please input valid movie id!',
+    MOVIE_NOT_FOUND: 'Movie not found in database!',
+    GENRE_NOT_FOUND: 'Genre Not found in database!',
+    INVALID_GENRE_PROPERTY: 'Genre has invalid value property!',
+    INVALID_TYPE_PROPERTY: 'Invalid type of property value (movies or genre)!',
+    ID_ERROR: `Invalid id value, fill id property!`,
+
+    INVALID_USERNAME: `Username must not empty`,
+    INVALID_PASSWORD: `Password must not empty`,
+    INVALID_EMAIL: `Email must not empty`,
+    PASSWORD_MISMATCH: `Password mismatch`,
+
+    SUCCESS_CREATE_ACCOUNT: `Account Success Created`,
+    ACCOUNT_NOT_FOUND: `Account not found in database!`,
+    SUCCESS_LOGIN: `Success Login into web movie`,
+    ACCOUNT_EXISTS: `Account already exists`,
+    FILE_NOT_FOUND: 'File not found',
+
+    INVALID_ACCOUNT_NAME: `Invalid account name, please use email or username`
+
+}
